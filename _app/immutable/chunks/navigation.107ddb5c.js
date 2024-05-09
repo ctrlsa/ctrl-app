1b6048d9ab614afe2b68af72374503f748f82e3f
Omri Aroesti <omri.aroesti@n-alpha.com>
2024-05-09 12:11:54 +0300

    Updates

--- _app/immutable/chunks/navigation.107ddb5c.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./singletons.c3bf6b65.js";const a=o("goto"),n=o("on_navigate");export{a as g,n as o};
---
+++ _app/immutable/chunks/navigation.4e08aabf.js
@@ -0,0 +1,1 @@
+import{j as o}from"./singletons.5ca29e73.js";const a=o("goto"),n=o("on_navigate");export{a as g,n as o};
