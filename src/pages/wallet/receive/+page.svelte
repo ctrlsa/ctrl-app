528c1ded019014ed869a424ba516dc0c771e3721
Omri Aroesti <omri.aroesti@n-alpha.com>
2024-06-30 18:10:35 +0300

    Improve Receive screen
    Add share button, change popup and change layout

--- src/pages/wallet/receive/+page.svelte
+++ src/pages/wallet/receive/+page.svelte
@@ -2,27 +2,41 @@
   import Button from "$shared/ui/button/button.svelte";
   import type { Keypair } from "@solana/web3.js";
   import WebApp from "@twa-dev/sdk";
-  import { CopyIcon } from "lucide-svelte";
+  import { CopyIcon, Share2Icon } from "lucide-svelte";
   import { getContext, onMount } from "svelte";
   import { get } from "svelte/store";
   import Qrcode from "qrcode";
+  import toast from "svelte-french-toast";
 
   const senderKp = get(getContext('keypair')) as Keypair;
+  $: kp = senderKp.publicKey.toString();
   let canvas: HTMLCanvasElement;
   function copy() {
-    navigator.clipboard.writeText(senderKp.publicKey.toString());
-    WebApp.showPopup({ message: 'Copied to clipboard!' });
+    navigator.clipboard.writeText(kp);
+    toast.success('Copied to clipboard!');
   }
-  onMount(() => {
-    Qrcode.toCanvas(canvas, senderKp.publicKey.toString());
-  });
+  onMount(() => Qrcode.toCanvas(canvas, kp));
 
 </script>
-<p class="break-all text-center">
+<div class="break-all text-center w-2/3 m-auto">
+  <canvas on:click={copy} class="m-auto" bind:this={canvas} />
   <!-- svelte-ignore a11y-missing-attribute -->
   <a role="button" on:keydown={copy} tabindex="-1" on:click={copy}>{senderKp.publicKey}</a>
-  <Button on:click={copy} class="btn btn-square btn-xs align-top">
-    <CopyIcon />
-  </Button>
-</p>
-<canvas on:click={copy} class="m-auto" bind:this={canvas} />
+</div>
+<div class="flex justify-around">
+  <div class="flex flex-col items-center mt-4">
+    <Button on:click={copy} class="btn btn-circle align-top">
+      <CopyIcon />
+    </Button>
+    Copy
+  </div>
+  
+  <div class="flex flex-col items-center mt-4">
+    <Button class="btn btn-circle align-top" on:click={
+      () => WebApp.openTelegramLink(`https://t.me/share/url?url=${kp}`)
+    }>
+      <Share2Icon />
+    </Button>
+    Share
+  </div>
+</div>
