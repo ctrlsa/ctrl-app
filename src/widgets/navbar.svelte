9647d2fa7f95c7f56d7647728beec3f063e3f2b1
Omri Aroesti <omri.aroesti@n-alpha.com>
2024-06-30 17:44:50 +0300

    Remove "solana" in navbar

--- src/widgets/navbar.svelte
+++ src/widgets/navbar.svelte
@@ -1,15 +1,1 @@
-<script>
-  import { ChevronDown, ScanLine } from "lucide-svelte";
-  import Button from "$lib/ui/button/button.svelte";
-</script>
-
-<nav class="navbar sticky top-0 z-10 bg-base-100">
-  <div class="navbar-start">
-    <div class="dropdown ml-4">
-      <Button href="/wallet" class="btn btn-ghost m-0 p-0 text-2xl normal-case">
-        Solana
-        <ChevronDown class="text-neutral-500" />
-      </Button>
-    </div>
-  </div>
-</nav>
+<nav class="navbar sticky top-0 z-10 bg-base-100" />
