fc87e9bc0ce51375523296aa5de3e68f63002cb4
Omri Aroesti <omri.aroesti@n-alpha.com>
2024-07-09 09:54:26 +0300

    Add https support for local env development

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import mkcert from 'vite-plugin-mkcert'
 
 /** @type {import('@sveltejs/kit').Config} */
 export default defineConfig({
-  plugins: [sveltekit(), nodePolyfills({include: ['buffer']})],
+  plugins: [sveltekit(), nodePolyfills({include: ['buffer']}), mkcert()],
+  server: {
+    https: true,
+    host: '0.0.0.0',
+  },
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"]
   }
